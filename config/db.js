3b48e13897b92121705e823f7d0f1dd150464285
vophammy24 <vophammy@gmail.com>
2026-06-17 09:00:14 +0700

    Update

--- config/db.js
+++ config/db.js
@@ -9,12 +9,15 @@ const connectDB = async () => {
     }
 
     const MONGO_URI = process.env.MONGO_URI;
+    const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "assignment2";
 
     if (!MONGO_URI) {
       throw new Error("MONGO_URI is not defined");
     }
 
-    const connect = await mongoose.connect(MONGO_URI);
+    const connect = await mongoose.connect(MONGO_URI, {
+      dbName: MONGO_DB_NAME
+    });
 
     isConnected = true;
 
